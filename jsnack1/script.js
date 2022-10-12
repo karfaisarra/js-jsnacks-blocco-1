fa87f5325935894766d8eb8e4d78b5ea0b0109a9
karfaisarra <karfaisarra@gmail.com>
2022-10-12 11:43:49 +0200

    show result commit

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,12 +4,19 @@
 const firstUser = prompt('Inserisci il primo numero');
 const secondUser = prompt('Inserisci il secondo numero');
 
+let result;
 if(firstUser > secondUser){
     console.log(firstUser);
+    result = firstUser;
 }else if (firstUser < secondUser){
     console.log(secondUser);
+    result = secondUser;
+
 }else {
     console.log('sono uguali');
+    result = 'sono uguali';
 }
 
 //Il software stampa il maggiore.
+const h1El = document.querySelector('h1');
+h1El.innerHTML=`${result}`;
