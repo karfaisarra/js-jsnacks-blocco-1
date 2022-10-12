da4c5838f22aa3d811e98b8c1b0187a70dee0479
karfaisarra <karfaisarra@gmail.com>
2022-10-12 12:50:34 +0200

    correzione

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,7 +1,7 @@
 /* Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti */
 
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
-const parola1 = Number(prompt('Inserisci un numero'));
+/* const parola1 = Number(prompt('Inserisci un numero'));
 const parola2 = Number(prompt('Inserisci un numero'));
 const parola3 = Number(prompt('Inserisci un numero'));
 const parola4 = Number(prompt('Inserisci un numero'));
@@ -15,4 +15,16 @@ const parola10 = Number(prompt('Inserisci un numero'));
 const result = parola1 + parola2 + parola3 + parola4 + parola5 + parola6 + parola7 + parola8 + parola9 + parola10 ;
 //stampo il risultato
 const h1El = document.querySelector('h1');
-h1El.innerHTML=`${result}`;
+h1El.innerHTML=`${result}`; */
+
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+let sum = 0;
+for (let i = 0; i < 10 ; i++) {
+    const userNumber = Number(prompt('Inserisci un numero'));
+    sum += userNumber;
+}
+console.log(sum);
+
+//stampo il risultato
+const h1El = document.querySelector('h1');
+h1El.innerHTML=`${sum}`;
